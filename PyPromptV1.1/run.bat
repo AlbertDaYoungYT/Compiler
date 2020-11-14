a569ca0d85d4a45e1f29c26efa562936943c29fa
AlbertDaYoungYT <65977153+AlbertDaYoungYT@users.noreply.github.com>
2020-11-14 20:08:08 +0100

    Update run.bat

--- PyPromptV1.1/run.bat
+++ PyPromptV1.1/run.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Welcome to PyPrompt v1.1
 echo Type "help" for more information.
-"C:\Python39\python.exe" "I:\Python\PyPrompt1.1\main.py"
+"C:\Python39\python.exe" "C:\PyPrompt1.1\main.py"
 pause
