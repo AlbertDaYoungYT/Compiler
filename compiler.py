57e36c795ee09f80520d169842f36331b2e53148
AlbertDaYoungYT <65977153+AlbertDaYoungYT@users.noreply.github.com>
2022-04-25 11:34:07 +0200

    push

--- compiler.py
+++ compiler.py
@@ -1,3 +1,20 @@
+import json
 
+global config
+with open("./config.json") as f:config = json.load(f)
 
-print("test")
+def WriteCompiledLine(line):
+    WriteLine = ""
+    for x in line:
+        WriteLine = ''.join(x)
+    WriteLine = WriteLine[:-1]
+    open(out, "a", encoding="utf-8").write(WriteLine + "\n")
+
+def ParseLine(line):
+    line.split(config["Splitter"])
+    for x in line:
+        for y in range(len(config["DataTypesFrom"])):
+            x.replace(config["DataTypesFrom"][y], config["DataTypesTo"][y])
+        for y in range(len(config["OperatorsFrom"])):
+            x.replace(config["OperatorsFrom"][y], config["OperatorsTo"][y])
+        
